6698f1a8e7490eefe4e6a0a76a22d3e9e5887029
0x384c0 <0x384c0@gmail.com>
2019-08-04 18:24:10 +0600

    code injection

--- research/README.md
+++ research/README.md
@@ -1,8 +1,13 @@
-* find way to acces array of functions
-* find way to identify and acces LayerExplosion and PhaseAttackNormal
-* override PhaseAttackNormal to empty task
-* create custom PhaseAttackNormal
-* override calling of construcotr PhaseAttackNormal(i,n,o,r,s,c,h)
-* draw red sqare on attack
+* done: find way to acces array of functions
+* done: find way to identify and acces LayerExplosion and PhaseAttackNormal
+* done: override PhaseAttackNormal to empty task
+* done: create custom PhaseAttackNormal
+* done: draw sqare on attack
+* done: play sound
 * create extension
-* TBA...
+* inject code without restart
+* hot reload
+* resource override and inject
+* explosion form AOW
+* attack from AOW
+* sfx from AOW
--- research/kc_analysys_comments.js
+++ research/kc_analysys_comments.js
@@ -1,49 +1,157 @@
-SE.play("102") 
+// SE.play("102") 
 
-//air battle fighter
-l.SE.play("102"), //1
+// //air battle fighter
+// l.SE.play("102"), //1
 
-//explosion on ship
-e.prototype.playExplosionSmall = 
+// //explosion on ship
+// e.prototype.playExplosionSmall = 
 
-//night double
-e.prototype._attack = function(t, e) { //3
+// //night double
+// e.prototype._attack = function(t, e) { //3
 
-// attack playe fleet
-e.prototype._attack = function(t, e) { //1
+// // attack playe fleet
+// e.prototype._attack = function(t, e) { //1
 
-//
-e.prototype._attack = function(t, e) { //4
+// //
+// e.prototype._attack = function(t, e) { //4
 
-//double attack first
-e.prototype._doubleH = function() {
-//double attack second
-v._cutin.view.once("attack", function() {
-
-
-
-//ship sunk PVP
-v._completeDamageEffect = function() {
+// //double attack first
+// e.prototype._doubleH = function() {
+// //double attack second
+// v._cutin.view.once("attack", function() {
 
 
 
-//attack start
-e.prototype.attack = function(t) {
+// //ship sunk PVP
+// v._completeDamageEffect = function() {
 
-//common attack  
-createjs.Tween.get(null).wait(r).call(function() {
 
-//Explosion depends of damage
-e.prototype.playDamageExplosion = function(t, e, i, n) {
 
-//init attack cnimation
-//i - _scene texture bg_map/bg_h.png
-//n - attacker
-//o - target
-var y = new a.PhaseAttackNormal(i,n,o,r,s,c,h);
+// //attack start
+// e.prototype.attack = function(t) {
 
-//PhaseAttackNormal constructor
-function e(e, i, n, o, s, a, _) {
+// //common attack  
+// createjs.Tween.get(null).wait(r).call(function() {
 
-//generate animation task for surface battle
-e.prototype._hougekiCycle = function() {//2
+// //Explosion depends of damage
+// e.prototype.playDamageExplosion = function(t, e, i, n) {
+
+// //init attack cnimation
+// //i - _scene texture bg_map/bg_h.png
+// //n - attacker
+// //o - target
+// var y = new a.PhaseAttackNormal(i,n,o,r,s,c,h);
+
+// //PhaseAttackNormal constructor
+// function e(e, i, n, o, s, a, _) {
+
+// //generate animation task for surface battle
+// e.prototype._hougekiCycle = function() {//2
+
+
+
+
+
+
+
+//prevent load main.js
+//prevent run KCS.init()
+var elem = document.querySelector('body > canvas');elem.parentNode.removeChild(elem);
+fetch("./js/main.js?version=4.4.2.5")
+.then(b => b.text())
+.then(s => s.replace("e.PhaseAttackNormal=_", "document.kcs_PhaseAttackNormal = e;e.PhaseAttackNormal=_"))
+.then(s => s.replace("e.LayerExplosion=u", "document.kcs_LayerExplosion = e;e.LayerExplosion=u"))
+.then(s => s.replace("!function(t){function e(t){null!=t&&", "document.kcs_SE = e;!function(t){function e(t){null!=t&&"))
+.then(s => eval(s))
+
+
+class CustomPhaseAttackNormal extends document.kcs_PhaseAttackNormal.PhaseAttackNormal {
+   _playExplosion(t, e) {
+        var i = t.getGlobalPos(!0);
+        this._scene.view.layer_explosion.playDamageExplosionCustom(i.x, i.y, e)
+    }
+}
+document.kcs_PhaseAttackNormal.PhaseAttackNormal = CustomPhaseAttackNormal
+
+
+class CustomExplosion extends PIXI.Container{
+	constructor(){
+		super();
+	    this._current_frame = 0
+	    this._img = new PIXI.Sprite
+	    this.addChild(this._img)
+	}
+
+
+	play(completion){
+        void 0 === completion && (completion = null),
+        this._isPlaying || (this._isPlaying = !0,
+        this.createTween(completion))
+	}
+	createTween (completion){
+        var e = this;
+        void 0 === completion && (completion = null),
+        createjs.Tween.removeTweens(this),
+        this._tween = createjs.Tween.get(this),
+        this._tween.wait(33).call(function() {
+            e._current_frame++,
+            e._img.texture = e._getTexture(e._current_frame),
+            e._setImageOffset(e._current_frame),
+            e._current_frame <= 15 ? e.createTween(completion) : (e._isPlaying = !1,
+            e._current_frame = 0,
+            null != completion && completion())
+        })
+	}
+	stop(){
+        this._isPlaying && (this._isPlaying = !1,
+        this._tween.setPaused(!0)),
+        this._current_frame = 0,
+        this._img.texture = this._getTexture(this._current_frame),
+        this._setImageOffset(this._current_frame)
+	}
+
+	_getTexture(current_frame) {
+		return PIXI.Texture.WHITE
+	}
+
+	_setImageOffset(current_frame) {
+        this._img.position.set(0, 0);
+	}
+}
+
+class CustomLayerExplosion extends document.kcs_LayerExplosion.LayerExplosion {
+    playDamageExplosionCustom(t, e, i, n) {
+        void 0 === n && (n = null),
+            i < 0 ? null != n && n() : i < 16 ? this.playExplosionLargeCustom(t, e, n) : i < 40 ? this.playExplosionLargeCustom(t, e, n) : this.playExplosionLargeCustom(t, e, n)
+    }
+
+    playExplosionLargeCustom(t, e, i) {
+        var n = this;
+        void 0 === i && (i = null),
+            createjs.Tween.get(this).call(function () {
+                document.kcs_SE.SE.play("104"),
+                    n._explodeCustom(t - 5, e + 33)
+            }).wait(100).call(function () {
+                n._explodeCustom(t + 48, e - 24)
+            }).wait(100).call(function () {
+                n._explodeCustom(t - 50, e - 8, i)
+            })
+    }
+
+    _explodeCustom(t, e, i) {
+            var n = this;
+            void 0 === i && (i = null);
+            var o = new CustomExplosion();
+            o.position.set(t, e),
+                this.addChild(o),
+                o.play(function () {
+                    n.removeChild(o),
+                        null != i && i()
+                })
+        }
+}
+document.kcs_LayerExplosion.LayerExplosion = CustomLayerExplosion
+
+
+
+KCS.init()
