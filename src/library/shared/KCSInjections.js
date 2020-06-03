3e08980a0b2e8a4f8ba216db62639a9b53e5eb4b
0x384c0 <0x384c0@gmail.com>
2020-06-03 16:49:26 +0600

    new possible injection

--- src/library/shared/KCSInjections.js
+++ src/library/shared/KCSInjections.js
@@ -1,4 +1,5 @@
-async function injectKCSMods(extensionUrl) {
+async function _injectKCSMods(extensionUrl) {
+    /*
     let scriptUrl = _findScript("js/main.js").src
     let scripts = []
     //load and modify main script
@@ -11,6 +12,21 @@ async function injectKCSMods(extensionUrl) {
         .then(s => s.replace(/(.)\.PhaseAttackDouble=(.)/, "document.kcs_PhaseAttackDouble = $1;e.PhaseAttackDouble=$2"))
         .then(s => s.replace(/(.)\.PhaseAttackNormal=(.)/, "document.kcs_PhaseAttackNormal = $1;e.PhaseAttackNormal=$2"))
     eval(mainScript)
+    */
+   
+   /*
+   var variable = () => {"asdas"}
+
+    Object.defineProperty(Object.prototype, 'KCS',{ configurable: true, 
+    get: () => { console.log( "gettter called");return "getted value"},
+    set: () => { console.log( "setter called");}
+    })
+
+    // delete Object.prototype.KCS
+
+    variable['KCS'] = "asdasd"
+    console.log(variable.KCS)
+   */
 
     //get classes
     await _addScript(extensionUrl + "library/shared/CustomSoundManager.js")
@@ -55,13 +71,26 @@ function _findScript(src){
     return Array.prototype.slice.call(document.querySelectorAll("script")).find(s => (s.src != null && s.src.includes(src)))
 }
 
-Object.defineProperty(window, 'KCS',{ configurable: true, value: function(){/*"KCS empty define"*/}})
-KCS.init = () => {/*"KCS empty init"*/}
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    //prevent init main.js
-    let extensionUrl = _findScript("KCSInjections.js").innerText.replace("'","").replace("'","")
+var _kcsBackup;
+function _disableGameInit(){
+    Object.defineProperty(window, 'KCS',{ configurable: true, 
+        get: () => {return {init:() => {console.log("KCS.init() was skipped")}}},
+        set: (kcs) => {_kcsBackup = kcs}
+    })
+}
+
+function _enableGameInit(){
     delete window.KCS
+    window.KCS = _kcsBackup
+}
+
+
+_disableGameInit()
+window.addEventListener('DOMContentLoaded', (event) => { 
+    _enableGameInit()
+
     //inject code
-    injectKCSMods(extensionUrl)
+    let extensionUrl = _findScript("KCSInjections.js").innerText.replace("'","").replace("'","")
+    _injectKCSMods(extensionUrl)
 });
